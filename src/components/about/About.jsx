27c5ed7869355f328665f3e42be0b4627268be5d
Izora9099 <ndifonlemuel@gmail.com>
2023-05-27 13:31:54 +0100

    Commit changes

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
 import './About.css'
+import ME from '../../assets/changer.png'
 const about = () => {
   return (
-   <section id="about">About</section>
+   <section id="about">
+    <h5>Get To Know</h5>
+    <h2>About Me</h2>
+    <div className="container about__container">
+      <div className="about__me">
+<div className="about__me-image">
+  <img src="" alt="" />
+</div>
+      </div>
+      <div className="about__content"></div>
+    </div>
+   </section>
   )
 }
 
