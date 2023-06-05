fc655f1232937648096d88ce5d5bb393c8715614
Izora9099 <ndifonlemuel@gmail.com>
2023-06-05 11:12:36 +0100

    commit

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -70,40 +70,16 @@ const Portfolio = () => {
   return (
     <section id="portfolio">
       <h5>My Recent Work</h5>
-      <h2>Portfolio</h2>
 
-      <div className="container portfolio-container">
-        {data.map(({ id, image, title, github, demo }) => {
-          return (
-            <div className="portfolio-box">
-              <article key={id} className="portfolio__item ">
-              <div className="portfolio__item-image portfolio-layer1">
-                <img src={image} alt={title} />
-              </div>
-              <h3>{title}</h3>
-              <div className="portfolio__item-cta">
-                <a href={github} className="btn" target="_blank">
-                  GitHub
-                </a>
-                <a href={demo} target="_blank" className="btn btn-primary">
-                  Live Demo
-                </a>
-              </div>
-              </article>
-            </div>
-                      );
-        })}
-      </div>
       <div class='portfolio-container'>
         <div class='portfolio-box'>
           <img src={me} alt='my image' />
           <div class='portfolio-layer'>
             <h4>Digital Art</h4>
             <p>
-              Lorem, ipsum dolor sit amet consectetur axime sed error impedit
-              nisi quas.
+              Transform your space with captivating digital art
             </p>
-            <div className='see-more'>See More!</div>
+            <div className='  see-more btn ctn'>See More!</div>
           </div>
         </div>
         <div class='portfolio-box'>
@@ -111,10 +87,9 @@ const Portfolio = () => {
           <div class='portfolio-layer'>
             <h4>Digital Art</h4>
             <p>
-              Lorem, ipsum dolor sit amet consectetur axime sed error impedit
-              nisi quas.
+              Transform your space with captivating digital art
             </p>
-            <div className='see-more'>See More!</div>
+            <div className='  see-more btn'>See More!</div>
           </div>
         </div>
         <div class='portfolio-box'>
@@ -122,10 +97,9 @@ const Portfolio = () => {
           <div class='portfolio-layer'>
             <h4>Digital Art</h4>
             <p>
-              Lorem, ipsum dolor sit amet consectetur axime sed error impedit
-              nisi quas.
+              Transform your space with captivating digital art
             </p>
-            <div className='see-more'>See More!</div>
+            <div className='  see-more btn'>See More!</div>
           </div>
         </div>
         <div class='portfolio-box'>
@@ -133,10 +107,9 @@ const Portfolio = () => {
           <div class='portfolio-layer'>
             <h4>Digital Art</h4>
             <p>
-              Lorem, ipsum dolor sit amet consectetur axime sed error impedit
-              nisi quas.
+              Transform your space with captivating digital art
             </p>
-            <div className='see-more'>See More!</div>
+            <div className='  see-more btn'>See More!</div>
           </div>
         </div>
 
@@ -145,10 +118,9 @@ const Portfolio = () => {
           <div class='portfolio-layer'>
             <h4>Digital Art</h4>
             <p>
-              Lorem, ipsum dolor sit amet consectetur axime sed error impedit
-              nisi quas.
+              Transform your space with captivating digital art
             </p>
-            <div className='see-more'>See More!</div>
+            <div className='  see-more btn'>See More!</div>
           </div>
         </div>
 
@@ -157,10 +129,9 @@ const Portfolio = () => {
           <div class='portfolio-layer'>
             <h4>Digital Art</h4>
             <p>
-              Lorem, ipsum dolor sit amet consectetur axime sed error impedit
-              nisi quas.
+              Transform your space with captivating digital art
             </p>
-            <div className='see-more'>See More!</div>
+            <div className='  see-more btn'>See More!</div>
           </div>
         </div>
         <div class='portfolio-box'>
@@ -168,10 +139,9 @@ const Portfolio = () => {
           <div class='portfolio-layer'>
             <h4>Digital Art</h4>
             <p>
-              Lorem, ipsum dolor sit amet consectetur axime sed error impedit
-              nisi quas.
+              Transform your space with captivating digital art
             </p>
-            <div className='see-more'>See More!</div>
+            <div className='  see-more btn'>See More!</div>
           </div>
         </div>
         
@@ -180,10 +150,9 @@ const Portfolio = () => {
           <div class='portfolio-layer'>
             <h4>Digital Art</h4>
             <p>
-              Lorem, ipsum dolor sit amet consectetur axime sed error impedit
-              nisi quas.
+              Transform your space with captivating digital art
             </p>
-            <div className='see-more'>See More!</div>
+            <div className='  see-more btn'>See More!</div>
           </div>
         </div>
         <div class='portfolio-box'>
@@ -191,10 +160,9 @@ const Portfolio = () => {
           <div class='portfolio-layer'>
             <h4>Digital Art</h4>
             <p>
-              Lorem, ipsum dolor sit amet consectetur axime sed error impedit
-              nisi quas.
+              Transform your space with captivating digital art
             </p>
-            <div className='see-more'>See More!</div>
+            <div className='  see-more btn'>See More!</div>
           </div>
         </div>{" "}
         <div class='portfolio-box'>
@@ -202,10 +170,9 @@ const Portfolio = () => {
           <div class='portfolio-layer'>
             <h4>Digital Art</h4>
             <p>
-              Lorem, ipsum dolor sit amet consectetur axime sed error impedit
-              nisi quas.
+              Transform your space with captivating digital art
             </p>
-            <div className='see-more'>See More!</div>
+            <div className='  see-more btn'>See More!</div>
           </div>
         </div>
         <div class='portfolio-box'>
@@ -213,10 +180,9 @@ const Portfolio = () => {
           <div class='portfolio-layer'>
             <h4>Digital Art</h4>
             <p>
-              Lorem, ipsum dolor sit amet consectetur axime sed error impedit
-              nisi quas.
+              Transform your space with captivating digital art
             </p>
-            <div className='see-more'>See More!</div>
+            <div className='  see-more btn'>See More!</div>
           </div>
       </div>
       <div class='portfolio-box'>
@@ -224,10 +190,9 @@ const Portfolio = () => {
           <div class='portfolio-layer'>
             <h4>Digital Art</h4>
             <p>
-              Lorem, ipsum dolor sit amet consectetur axime sed error impedit
-              nisi quas.
+              Transform your space with captivating digital art
             </p>
-            <div className='see-more'>See More!</div>
+            <div className='  see-more btn'>See More!</div>
           </div>
       </div>
       
@@ -239,52 +204,3 @@ const Portfolio = () => {
 };
 
 export default Portfolio;
-
-
-/*
-import React from "react";
-import "./portfolio.css";
-import { FaExternalLinkAlt } from "react-icons/fa";
-import me from "../../assets/Fury.png";
-import me1 from "../../assets/B-W.png";
-import me2 from "../../assets/Cool girl.png";
-import me3 from "../../assets/Raindrops.png";
-import me4 from "../../assets/Shawty.png";
-import me5 from "../../assets/MY CRUSH DISPLAY 2.png";
-import me6 from "../../assets/photo_2023-03-26_10-57-34.jpg";
-import me7 from "../../assets/Ms Ajax.png";
-import me8 from "../../assets/BABY DISPLAY.png";
-import me9 from "../../assets/BEAUTY QUEEN2.png";
-import me10 from "../../assets/happy mothers day display 3.png";
-import me11 from "../../assets/01.png";
-import me12 from "../../assets/clean.png";
-
-import img1 from "../../assets/img1.png";
-import img2 from "../../assets/img2.png";
-import img3 from "../../assets/img3.png";
-import img5 from "../../assets/img5.png";
-import img6 from "../../assets/img6.png";
-import img7 from "../../assets/img7.png";
-import img8 from "../../assets/img8.png";
-import img9 from "../../assets/img9.png";
-
-
-const Portfolio = () => {
-   
-
-  return (
-    <section class='portfolio' id='portfolio'>
-      <h5>See My Work</h5>
-      <h2 class='heading'>
-        Latest <span>Projects</span>
-      </h2>
-      <h3 className='section-head'>Digital Art</h3>
-      
-       
-    
-    </section>
-  );
-};
-
-export default Portfolio;
-*/
--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./services.css";
-import { MdOutlineLibraryAddCheck } from "react-icons/md";
+import { BsPatchCheckFill } from "react-icons/bs";
 
 export const Services = () => {
   return (
@@ -16,43 +16,43 @@ export const Services = () => {
 
           <ul className='service__list'>
             <li>
-              <MdOutlineLibraryAddCheck className='service__list-icon' />
+              <BsPatchCheckFill className='service__list-icon' />
               <p>
                 Creating content for your social media pages like TikTok,
                 Instagram, Facebook
               </p>
             </li>
             <li>
-              <MdOutlineLibraryAddCheck className='service__list-icon' />
+              <BsPatchCheckFill className='service__list-icon' />
               <p>I create content that captivates and engages.</p>
             </li>
             <li>
-              <MdOutlineLibraryAddCheck className='service__list-icon' />
+              <BsPatchCheckFill className='service__list-icon' />
               <p>
                 I can help you reach your target audience with engaging content.
               </p>
             </li>
             <li>
-              <MdOutlineLibraryAddCheck className='service__list-icon' />
+              <BsPatchCheckFill className='service__list-icon' />
               <p>I'm a creative thinker who can come up with original ideas.</p>
             </li>
             <li>
-              <MdOutlineLibraryAddCheck className='service__list-icon' />
+              <BsPatchCheckFill className='service__list-icon' />
               <p> Proficient in writing and editing across various mediums</p>
             </li>
             <li>
-              <MdOutlineLibraryAddCheck className='service__list-icon' />
+              <BsPatchCheckFill className='service__list-icon' />
               <p>Knowledge of storytelling and narrative structure</p>
             </li>
             <li>
-              <MdOutlineLibraryAddCheck className='service__list-icon' />
+              <BsPatchCheckFill className='service__list-icon' />
               <p>
                 Ability to create engaging content tailored to specific
                 audiences
               </p>
             </li>
             <li>
-              <MdOutlineLibraryAddCheck className='service__list-icon' />
+              <BsPatchCheckFill className='service__list-icon' />
               <p>Experience with social media and marketing materials</p>
             </li>
           </ul>
@@ -66,37 +66,37 @@ export const Services = () => {
 
           <ul className='service__list'>
             <li>
-              <MdOutlineLibraryAddCheck className='service__list-icon' />
+              <BsPatchCheckFill className='service__list-icon' />
               <p>I build websites that are both beautiful and functional.</p>
             </li>
             <li>
-              <MdOutlineLibraryAddCheck className='service__list-icon' />
+              <BsPatchCheckFill className='service__list-icon' />
               <p>
                 I'm a web developer who loves to create user-friendly
                 experiences.
               </p>
             </li>
             <li>
-              <MdOutlineLibraryAddCheck className='service__list-icon' />
+              <BsPatchCheckFill className='service__list-icon' />
               <p>I'm passionate about using technology to solve problems.</p>
             </li>
             <li>
-              <MdOutlineLibraryAddCheck className='service__list-icon' />
+              <BsPatchCheckFill className='service__list-icon' />
               <p>Proficient in HTML, CSS, and JavaScript</p>
             </li>
             <li>
-              <MdOutlineLibraryAddCheck className='service__list-icon' />
+              <BsPatchCheckFill className='service__list-icon' />
               <p>Knowledge of UI/UX design principles </p>
             </li>
             <li>
-              <MdOutlineLibraryAddCheck className='service__list-icon' />
+              <BsPatchCheckFill className='service__list-icon' />
               <p>
                 Familiarity with responsive design and cross-browser
                 compatibility
               </p>
             </li>
             <li>
-              <MdOutlineLibraryAddCheck className='service__list-icon' />
+              <BsPatchCheckFill className='service__list-icon' />
               <p>Ability to collaborate with clients and stakeholders</p>
             </li>
           </ul>
@@ -111,38 +111,38 @@ export const Services = () => {
 
           <ul className='service__list'>
             <li>
-              <MdOutlineLibraryAddCheck className='service__list-icon' />
+              <BsPatchCheckFill className='service__list-icon' />
               <p> Proficient in digital cartooning and digital painting</p>
             </li>
             <li>
-              <MdOutlineLibraryAddCheck className='service__list-icon' />
+              <BsPatchCheckFill className='service__list-icon' />
               <p>
                 Expertise in framing pictures and professional picture
                 enhancement
               </p>
             </li>
             <li>
-              <MdOutlineLibraryAddCheck className='service__list-icon' />
+              <BsPatchCheckFill className='service__list-icon' />
               <p>Knowledge of cutting-edge software and tools</p>
             </li>
             <li>
-              <MdOutlineLibraryAddCheck className='service__list-icon' />
+              <BsPatchCheckFill className='service__list-icon' />
               <p>Collaborated with clients to bring their vision to life</p>
             </li>
             <li>
-              <MdOutlineLibraryAddCheck className='service__list-icon' />
+              <BsPatchCheckFill className='service__list-icon' />
               <p>
                 Delivered high-quality work that exceeded client expectation
               </p>
             </li>
             <li>
-              <MdOutlineLibraryAddCheck className='service__list-icon' />
+              <BsPatchCheckFill className='service__list-icon' />
               <p>
                 Worked on a variety of projects, from small-scale to large-scale
               </p>
             </li>
             <li>
-              <MdOutlineLibraryAddCheck className='service__list-icon' />
+              <BsPatchCheckFill className='service__list-icon' />
               <p>
                 Attention to detail and ability to work to specific client needs
               </p>
