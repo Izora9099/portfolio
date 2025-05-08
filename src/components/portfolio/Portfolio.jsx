b9bc895d0511236c6ed392b592643206dd1e6404
Izora9099 <ndifonlemuel@gmail.com>
2025-05-08 03:57:00 +0100

    portfolio project backend implemented, data now read directly from appwrite

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -16,10 +16,11 @@ const Portfolio = () => {
         const response = await appwriteService.listDocuments('681c086300399ca68598', '681c087e0031be6c71f1');
         const formattedData = response.documents.map(item => ({
           id: item.$id,
-          image: item.image,
+          image: item.imageURL,
           title: item.title,
-          content: item.content,
-          seemore: item.seemore
+          description: item.description,
+          techStack: Array.isArray(item.techStack) ? item.techStack : [],
+          projectLink: item.projectLink
         }));
         setData(formattedData);
       } catch (error) {
@@ -53,6 +54,10 @@ const Portfolio = () => {
     );
   }
 
+  /*
+  for getting the imageURL, just navigate to the file in the bucket in appwrite and copy and paste, simple
+  
+  */
   return (
     <section id='portfolio'>
       <h5>My recent work</h5>
@@ -60,24 +65,29 @@ const Portfolio = () => {
 
       <div class='portfolio-container'>
         {
-        data.map(({ id, image, title, content, seemore }) => {
+        data.map(({ id, image, title, description, techStack, projectLink }) => {
           return (
-            <div className='portfolio-box'>
-              <article key={id} className=''>
+            <div key={id} className='portfolio-box'>
+              <article className=''>
                 <div className='portfolio__item__image'>
                   <img src={image} alt={title} className='portImage' />
                 </div>
                 <div className='portfolio-layer'>
-                  <h3> {title} </h3>
-                  <p>{content}</p>
+                  <h3>{title}</h3>
+                  <p>{description}</p>
+                  <div className='tech-stack'>
+                    {techStack.map((tech, index) => (
+                      <span key={index} className='tech-tag'>{tech}</span>
+                    ))}
+                  </div>
                   <div className='portfolio__item__cta'>
                     <a
-                      href={seemore}
+                      href={projectLink}
                       className='see-more btn'
                       target='_blank'
                       rel='noreferrer'
                     >
-                      See More!
+                      View Project
                     </a>
                   </div>
                 </div>
