01b908a30ac21073ffe79ada49893a58f7a27bf4
Izora9099 <ndifonlemuel@gmail.com>
2025-05-09 14:24:33 +0100

    mobile view is not responsive and has errors, preliminary attempt to solve.

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,19 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from 'react-router-dom';
 import "./navbar.css";
 
 const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
 
   return (
     <nav className='navbar'>
       <div className="logo">
         <NavLink to="/">
           <img src="/favicon.svg" alt="Ndifon Lemuel" />
-          <span>Ndifon Lemuel</span>
+          <span className="name-text">Ndifon Lemuel</span>
         </NavLink>
       </div>
 
-      <div className="link">
+      <button 
+        className={`hamburger ${isMenuOpen ? 'menu-open' : ''}`} 
+        onClick={toggleMenu} 
+        aria-label="Toggle menu"
+      >
+        <span></span>
+        <span></span>
+        <span></span>
+      </button>
+
+      <div className={`link ${isMenuOpen ? 'menu-open' : ''}`}>
         <NavLink
           to="/"
           className={({ isActive }) => isActive ? "active" : ""}
